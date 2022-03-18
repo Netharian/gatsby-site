9c1243a2b06aebc18093f72912c599e7d28ffb79
Lucas Melo <lucas.melo@frncubo.com>
2022-03-17 23:51:04 -0300

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import { StaticImage } from "gatsby-plugin-image";
 import * as React from "react";
 import Layout from "../components/layout";
 
@@ -5,6 +6,7 @@ const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage alt="The Lich King" src="../images/arthas.jpg" />
     </Layout>
   );
 };
